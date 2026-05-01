47fec8d1bc390a4a6ff149c0a3f0d895fd088f68
Akash Mukherjee <akashmukhopadhayay@gmail.com>
2026-05-01 09:45:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://cashpilot-frontend.onrender.com/',
   credentials: true
 }))
 app.use(express.json());
